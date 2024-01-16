343db7a81214362e8a28d6075a02b1e1e9dbe6f3
ItisCaleb <happycaleb1212@gmail.com>
2024-01-16 18:53:26 +0800

    Implement basic shape draw

---
+++ scene_builder/index.js
@@ -0,0 +1,136 @@
+
+let canvas = new fabric.Canvas('canvas')
+
+let oGridGroup;
+let image;
+let size = 25;
+draw_grid(size);
+
+canvas.on('mouse:wheel', function (opt) {
+  var delta = opt.e.deltaY;
+  var zoom = canvas.getZoom();
+  zoom *= 0.999 ** delta;
+  if (zoom > 20) zoom = 20;
+  if (zoom < 0.01) zoom = 0.01;
+  canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+  opt.e.preventDefault();
+  opt.e.stopPropagation();
+  draw_grid(size)
+});
+
+canvas.on('mouse:down', function (opt) {
+  var evt = opt.e;
+  if (evt.altKey === true) {
+    this.isDragging = true;
+    this.selection = false;
+    this.lastPosX = evt.clientX;
+    this.lastPosY = evt.clientY;
+  }
+});
+canvas.on('mouse:move', function (opt) {
+  if (this.isDragging) {
+    var e = opt.e;
+    var vpt = this.viewportTransform;
+    vpt[4] += e.clientX - this.lastPosX;
+    vpt[5] += e.clientY - this.lastPosY;
+    this.requestRenderAll();
+    this.lastPosX = e.clientX;
+    this.lastPosY = e.clientY;
+  }
+});
+canvas.on('mouse:up', function (opt) {
+  // on mouse up we want to recalculate new interaction
+  // for all objects, so we call setViewportTransform
+  this.setViewportTransform(this.viewportTransform);
+  this.isDragging = false;
+  this.selection = true;
+});
+
+
+box.onclick = () => {
+  const rect = new fabric.Rect({
+    width: 50, height: 50,
+    left: 100, top: 100,
+    fill: null, stroke: 'red'
+  })
+  rect.set('lockRotation', true)
+  canvas.add(rect)
+}
+
+circle.onclick = () => {
+  const circle = new fabric.Circle({
+    radius: 25,
+    left: 100, top: 100,
+    fill: null, stroke: 'red'
+  })
+  circle.set('lockRotation', true)
+  canvas.add(circle)
+}
+
+line.onclick = () => {
+  const line = new fabric.Line([50, 100, 200, 200], {
+    left: 170,
+    top: 150,
+    stroke: 'red'
+  })
+  canvas.add(line)
+}
+
+
+document.addEventListener('keydown', function (event) {
+  const key = event.key; // const {key} = event; ES6+
+  if (key === "Backspace" || key === "Delete") {
+    for (let obj of canvas.getActiveObjects()) {
+      canvas.remove(obj);
+    }
+    return false;
+  }
+});
+
+function draw_grid(grid_size) {
+  canvas.remove(oGridGroup)
+  const gridSize = grid_size
+  const width = canvas.getWidth();
+  const height = canvas.getHeight();
+  const lines = [];
+  const lineOption = { stroke: 'rgba(0,0,0,1)', strokeWidth: 1, selectable: false };
+  for (let i = Math.ceil(width / gridSize); i>=0;i--) {
+    lines.push(new fabric.Line([gridSize * i, 0, gridSize * i, height], lineOption));
+  }
+  for (let i = Math.ceil(height / gridSize); i>=0;i--) {
+    lines.push(new fabric.Line([0, gridSize * i, width, gridSize * i], lineOption));
+  }
+  oGridGroup = new fabric.Group(lines, { left: 0, top: 0 });
+  oGridGroup.set('selectable',false)
+  oGridGroup.set('hoverCursor','auto')
+  canvas.add(oGridGroup);
+  canvas.moveTo(oGridGroup, 1);
+}
+
+document.getElementById('imgLoader').onchange = function handleImage(e) {
+  canvas.remove(oGridGroup)
+  var reader = new FileReader();
+    reader.onload = function (event){
+      var imgObj = new Image();
+      imgObj.src = event.target.result;
+      imgObj.onload = function () {
+        var image = new fabric.Image(imgObj);
+        image.set({
+              angle: 0,
+              padding: 10,
+              cornersize:10,
+              opacity: 0.5
+        });
+        image.set('selectable',false)
+        image.set('hoverCursor','auto')
+        canvas.setWidth(this.width);
+        canvas.setHeight(this.height);
+        canvas.centerObject(image);
+        canvas.add(image);
+        canvas.moveTo(image, 0);
+        draw_grid(size);
+        canvas.renderAll();
+      }
+    }
+    reader.readAsDataURL(e.target.files[0]);
+  }
