19a1874cc3a46850e99f9b86d92823b0b7c9af5a
Yuankai619 <Yuankai_@outlook.com>
2024-01-20 05:27:02 +0800

    Add camera and test background
    Test background:
    add test_background.png to test camera move correctness
    
    Camera:
    Player.h: Rewrite the Player::update function to add boundary check logic to prevent the Player from exceeding the defined screen range, in order to prevent errors in other code (such as the camera). The default range is 2560 in width and 1440 in height. Add the functions float Player::getX() and float Player::getY() to obtain the x,y coordinates of the Player.
    
    Game.h: Publicly add void setPlayer(Player player), loadBackground(const std::string &backgroundPath), renderBackground(SDL_Renderer renderer, const Camera &cam), SDL_Window getWindow(), SDL_Renderer getRenderer(), Camera &getCamera(), Player *getPlayer(). Privately add int width, height; Camera camera, Player *currentPlayer = nullptr, SDL_Texture *backgroundTexture = nullptr.
    
    Game.cc: In the Game::init function, add initialization for Game::width, Game::height, and initialize Game::currentPlayer as a new Player() pointer, as well as initialize Game::camera = Camera(width, height, 5.0f), where 5.0f is the test magnification ratio for the camera. Add getPlayer function to return currentPlayer. Add getCamera function to return camera. Add setPlayer function to set currentPlayer as player. Add loadBackground to use ResourceManager::load to load test_background.png into SDL_Texture for backgroundTexture. Add renderBackground to render the backgroundTexture onto the screen, including boundary checks and using camera.apply to transform coordinates. Modify render, after SDL_RenderClear(Game::renderer); add renderBackground(Game::renderer, camera); to render the background image first after each render
    
    Sprite.cc: Update the content of the Sprite::render function, adding Camera &camera = Game::getCamera(); renderRect = camera.apply(renderer, renderRect); before SDL_RenderCopy to transform the Sprite's coordinates into the coordinates of the camera world.
    
    Camera: The Camera::update function transforms the camera's own coordinates based on the Player's coordinates, keeping the Player at the center of the screen. The Camera::apply function takes an SDL_Rect object and converts it into a coordinate system from the camera's perspective, then returns an SDL_Rect. Camera::setZoom sets the zoom. The Camera::getZoom function is used to obtain the zoom value. The Camera::getX function is used to obtain the x coordinate. The Camera::getY function is used to obtain the y coordinate.
    
    main.cc: In Game, add the logic to load the background image at the very beginning of the Game loop with loadBackground("test_background.png"). After Game::update(delta), add logic to get the Player and then update the camera's coordinates. The code is: Player *player = Game::getPlayer();
    if (player) {
    //update camera position
    Game::getCamera().update(player->getX(), player->getY());
    }
    
    Bug report: In resource_manager.cc, within the load function, the line: SDL_Texture* texture = SDL_CreateTextureFromSurface(Game::getRenderer(), surface);
    if (!surface) {
    printf("Error: Unable to create texture from: %s. SDL Error %s\n", resource.c_str(), SDL_GetError());
    return nullptr;
    } should be changed to SDL_Texture* texture = SDL_CreateTextureFromSurface(Game::getRenderer(), surface);
    if (!texture) {
    printf("Error: Unable to create texture from: %s. SDL Error %s\n", resource.c_str(), SDL_GetError());
    return nullptr;
    }

---
+++ src/misc/camera.h
@@ -0,0 +1,25 @@
+#ifndef CAMERA_H_
+#define CAMERA_H_
+
+#include <SDL2/SDL.h>
+
+class Camera {
+public:
+    // Default constructor
+    Camera() : x(0), y(0), view_width(0), view_height(0), zoom(1.0f) {}
+    Camera(int width, int height, float initialZoom) : x(0), y(0), view_width(width), view_height(height), zoom(initialZoom) {}
+    
+    void update(float targetX, float targetY);
+    SDL_Rect apply( SDL_Rect rect);
+    void setZoom(float zoomLevel);
+    float getZoom();
+    float getX();
+    float getY();
+private:
+    float x, y;
+    int view_width, view_height;
+    float zoom;
+
+};
+
+#endif
