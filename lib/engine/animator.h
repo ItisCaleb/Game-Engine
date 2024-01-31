c6b9b7379a5c2a54a6217954611b60666da13883
ItisCaleb <happycaleb1212@gmail.com>
2024-01-31 18:03:02 +0800

    Update animator

--- lib/engine/animator.h
+++ lib/engine/animator.h
@@ -4,31 +4,53 @@
 
 #include "fsm.h"
 
+#include <unordered_map>
+
 #include "engine/entity.h"
 #include "engine/timer.h"
+
+struct AnimeProperty{
+    int startIdx, endIdx;
+};
 
 class Animator: public FSM<Entity>{
         public:
             void enter(Entity *instance){
                 this->timer.start();
-                this->anim = startIdx;
             }
             FSM<Entity>* update(Entity *instance, float dt){
+                if(!play) return nullptr;
                 if(timer.getTicks() >= this->animTick){
                     instance->chooseCurrentSprite(anim);
                     this->anim++;
-                    if(this->anim > this->endIdx) 
-                        this->anim = this->startIdx;
+                    if(this->anim >= currentAnimation->endIdx) 
+                        this->anim = currentAnimation->startIdx;
                     this->timer.restart();
                 }
                 return nullptr;
             }
             void exit(Entity *instance){}
 
-            void setIdx(int startIdx, int endIdx){
-                this->startIdx = startIdx;
-                this->endIdx = endIdx;
-                this->anim = startIdx;
+            void addAnimation(std::string name, int num){
+                auto anime = new AnimeProperty();
+                anime->startIdx = last;
+                anime->endIdx = last + num;
+                last += num;
+                properties[name] = anime;
+            }
+
+            void playAnimation(std::string name){
+                auto tmp = properties.find(name);
+                if(tmp == properties.end()){
+                    printf("Animation '%s' is not found\n",name);
+                    return;
+                }
+                currentAnimation = properties[name];
+                this->anim = currentAnimation->startIdx;
+                play = true;
+            }
+            void stopAnimation(){
+                play = false;
             }
 
             void setAnimTick(int tick){
@@ -37,8 +59,11 @@ class Animator: public FSM<Entity>{
         private:
             Timer timer;
             int animTick = Timer::TICK_12FRAMES;
-            int startIdx, endIdx;
+            std::unordered_map<std::string, AnimeProperty*> properties;
+            AnimeProperty* currentAnimation;
+            bool play = false;
             int anim;
+            int last = 0;
     };
 
 #endif
